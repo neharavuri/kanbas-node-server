97938e835a2ce938cdda7bef40a1d9f9b905c2ba
Neha <ravuri.n@northeastern.edu>
2023-12-03 12:05:39 -0500

    a6

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 import express from "express";
+import session from "express-session";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
+import mongoose from "mongoose";
 import cors from "cors";
-import "./node_modules/dotenv/config.js"
+import "dotenv/config";
+import UserRoutes from "./users/routes.js";
+import "./node_modules/dotenv/config.js";
+mongoose.connect("mongodb://localhost:27017/kanbas");
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: true }));
+app.use(
+  cors({ credentials: true, origin: process.env.FRONTEND_URL, origin: true })
+);
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+  app.use(session(sessionOptions));  
+
 CourseRoutes(app);
 ModuleRoutes(app);
+UserRoutes(app);
 Lab5(app);
-app.listen(process.env.PORT || 4000);
+app.listen(4000);
